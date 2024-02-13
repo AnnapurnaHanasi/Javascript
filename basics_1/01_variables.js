6ea7686c2b79ed2c315d04f13453821f446a2a78
AnnapurnaHanasi <117972171+AnnapurnaHanasi@users.noreply.github.com>
2024-02-13 14:05:32 +0000

    datatypes

---
+++ basics_1/01_variables.js
@@ -0,0 +1,10 @@
+const accountId =123
+var var1 ="anna"
+let pass ="hanasi"
+city = "Dharwad"
+// accountId = 11 Error
+var1 = 'newname'
+pass ="newpass"
+let accountState //if don't assign any val it will be undefined
+console.log('--',accountId,var1,pass,city,accountState);
+console.table([accountId,var1,pass,city,accountState])
---
+++ basics_1/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all JS code as newer version
+// alert('HEllo') we are using nodejs, not browser
+console.log(3+3); //code readibility should be good
+console.log('HEllo');
+
+/*ECMA - ecma script maintains coding standard, how to take ip's and op's */
+/*Referrences - mozzrilla docs "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/body" */
+
+let name='anna'
+let age= 21
+let isalgebra = false
+
+
+/* premetive datatypes*/
+//number => 2 to the power of 53
+//bigInt
+// string => ""/ ''
+//boolean => true/false
+/*null =>standalone value (representaion of null value - means empty) */
+//undefined
+//symbol => unique (to find uniqueness)
+
+
+/* -----Non-premetive datatypes----*/
+//object 
+console.log(typeof "annapurna");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
