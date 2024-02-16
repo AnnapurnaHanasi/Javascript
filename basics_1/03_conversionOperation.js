82b2f5726e1e15ede8f5876e26adfe5afc145580
AnnapurnaHanasi <117972171+AnnapurnaHanasi@users.noreply.github.com>
2024-02-16 14:36:44 +0000

    conversion operations

---
+++ basics_1/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="33"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let boolno = 1
+let converbool = Boolean(boolno)
+console.log(converbool)
+//33 =>33 get's converted
+//33abc => NaN becs it cannot be converted
+//true => 1 i.e bool value
+
+//Number(variable), Boolean(variable),String(var) ->to convert 
+//value
+// *************** Operations*********************
+/* add, sub, mult,pow(**),div,mod*/
+let val =2
+let negval=-val
+console.log(val)
+console.log(negval)
+console.log('if it is string treate it as string an num is sttar with num')
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
